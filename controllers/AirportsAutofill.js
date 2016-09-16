f4b973ed8e7f94311fd000f3e76b11d6339f62f7
slooooowpanda <kanybek@list.ru>
2016-09-17 02:51:05 +0500

    Finished AirportsAutofill.js

--- controllers/AirportsAutofill.js
+++ controllers/AirportsAutofill.js
@@ -1,11 +1,15 @@
 var fs = require('fs')
-var coolobj = require('../Resource/Airports.json')['airports'];
+var coolObj = require('../Resource/Airports.json')['airports'];
 
 module.exports = function (req, res) {
     if(req.method == 'GET'){
         var queryLanguage = getQueryLanguage(req);
         var toRespond = search(req.params.query, queryLanguage);
-        res.send(toRespond);
+        var q = [];
+         toRespond.forEach(function (element) {
+             q.push(element[queryLanguage]);
+         });
+        res.send(q);
     }
 }
 
@@ -13,22 +17,15 @@ function getQueryLanguage(req){
     var letter = req.params.query.charCodeAt(0);
 
     if(((letter >= 1072) && (letter <= 1103)) || ((letter >= 1040) && (letter <= 1071))){
-        return "rus";
+        return "name_rus";
     }
     if(((letter >= 97) && (letter <= 122)) || ((letter >= 65) && (letter <= 90))){
-        return "eng";
+        return "name_eng";
     }
 }
 
 function search(word, queryLanguage) {
-    if(queryLanguage == "rus"){
-        return coolobj.filter(function (item) {
-            return item.name_rus.toLowerCase().indexOf(word.toLowerCase()) == 0;
-        });
-    }
-    if(queryLanguage == "eng"){
-        return coolobj.filter(function (item) {
-            return item.name_eng.toLowerCase().indexOf(word.toLowerCase()) == 0;
-        }
-    )};
+   return coolObj.filter(function (item) {
+       return item[queryLanguage].toLowerCase().indexOf(word.toLowerCase()) == 0;
+   });
 }
