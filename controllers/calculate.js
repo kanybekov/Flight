71d5ab1d43f28019bd7550899eb83ddfddb90553
slooooowpanda <kanybek@list.ru>
2016-09-24 02:08:58 +0500

    some kind of specifing model of calculation request

--- controllers/calculate.js
+++ controllers/calculate.js
@@ -1,5 +1,23 @@
 module.exports = function (req, res) {
-    if(req.method == 'GET'){
+    /*
+     occasion - что произошло [отмена - задержка - отказ = 012]
+     cityFrom/ cityTo -города
+     date - дата
+     airline - авиакомпания
+     warnTime - За сколько дней предупредила об отмене? 7, (7,14),14 = 012
+     altFlight - альт. рейс?
 
+     flightDelayTime - время задержки 3, (3,4) ,4 = 012
+     altFlightDelayTime - время задержки альтрейса
+  */
+
+    //отмена - occasion, cityFromTo, date, airline, warnTime, altFlight, altFlightDelayTime
+    //задержка - occasion, cityFromTo, date, airline, delayTime
+    //отказ - occasion, cityFromTo, date, airline, altFlightDelayTime
+
+    if(req.method == 'POST'){
+        req.on('data', function (data) {
+
+        })
     }
 }
