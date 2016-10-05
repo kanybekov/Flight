4fd9c91ff4dcbf3ecb9f88e61c7bb348785b2dd0
Vadim Starikov <starikov.vs@gmail.com>
2016-10-05 19:30:29 +0500

    fixes for airport change

--- helpers/checkAirports.js
+++ helpers/checkAirports.js
@@ -1,24 +1,20 @@
 var mongoose = require('mongoose');
 
 module.exports = function (parsed, callback) {
-    db.Airport.find({
-        "_id": mongoose.Types.ObjectId(parsed.cityFrom)
-    }, function (err, result) {
-        var airportFrom = result[0];
-        db.Airport.find({
-            "_id": mongoose.Types.ObjectId(parsed.cityTo)
-        }, function (err, result) {
-            var airportTo = result[0];
-            db.Airline.find({
-                    "_id": mongoose.Types.ObjectId(parsed.airline)
-                },
-                function (err, result) {
-                    var airline = result[0];
-                    if ((airportFrom.is_europe == 1 || (airportTo.is_europe == 1 && airline.is_europe == 1))) {
-                        callback(true);
-                    }
-                    callback(false);
-                })
-        });
-    })
-}
+    db.Airport.find({"_id": mongoose.Types.ObjectId(parsed.cityFrom)},
+        function (err, result) {
+            var airportFrom = result[0];
+            db.Airport.find({
+                "_id": mongoose.Types.ObjectId(parsed.cityTo)
+            }, function (err, result) {
+                var airportTo = result[0];
+                db.Airline.find({
+                        "_id": mongoose.Types.ObjectId(parsed.airline)
+                    },
+                    function (err, result) {
+                        var airline = result[0];
+                        callback(airportFrom.is_europe == 1 || (airportTo.is_europe == 1 && airline.is_europe == 1))
+                    })
+            });
+        })
+};
