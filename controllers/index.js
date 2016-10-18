6e3b3545419f3950bbc0f8eee316d4cb7f6ef3a8
slooooowpanda <kanybek@list.ru>
2016-10-18 20:01:24 +0500

    Using new route

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ var app = require('express'),
     router = app.Router();
 
 router.use('/airport', require('./airport'));
-router.use('/airline', require('./airline'));
+router.use('/1/classes/airlineOther', require('./airline'));
 router.use('/calculator', require('./calculate'));
 
 module.exports = router;
