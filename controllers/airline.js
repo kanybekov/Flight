2c144270db3dcca70fd79b2489a40a7a6dbf46b8
slooooowpanda <kanybek@list.ru>
2016-10-18 19:23:35 +0500

    Now using "airports" collection + adapted airlines dropdown api

--- controllers/airline.js
+++ controllers/airline.js
@@ -4,11 +4,30 @@ var express = require('express'),
 
 router
     .post('/', function (req, res, next) {
-        var query = req.body;
-        db.Airline.find(query, function (err, airlines) {
-            console.log(airlines);
+        var fullRequest = req.body;
+        var limitAmount = fullRequest.limit;
+        var query = fullRequest.where;
+        var finalQuery = {};
+
+        var arr = [];
+
+        var q1 = {}, q2 = {}, q3  = {}, q4 = {};
+
+        q1.Name = query["$or"][0].Name;
+        q2.alias = query["$or"][1].alias;
+        q3.Callsign = query["$or"][2].Callsign;
+        q4.IATA = query["$or"][3].IATA;
+
+        arr.push(q1);
+        arr.push(q2);
+        arr.push(q3);
+        arr.push(q4);
+
+        finalQuery["$or"] = arr;
+
+        db.Airline.find(finalQuery, function (err, airlines) {
             res.send(airlines);
-        })
+        }).limit(limitAmount);
     })
 
 module.exports = router;
